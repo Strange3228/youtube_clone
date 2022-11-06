cca2104d2c65b0415d09ef377703de2279f0536b
Strange3228 <Mandarin753951a@gmail.com>
2022-11-06 13:31:40 +0100

    Relative Videos

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -2,18 +2,24 @@ import { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import ReactPlayer from "react-player";
 import { Typography, Box, Stack } from "@mui/material";
-import { CheckCircle } from "@mui/icons-material";
+import { CheckCircle, SettingsInputSvideoSharp } from "@mui/icons-material";
 
-import { Video } from "./";
+import { Videos } from "./";
 import { FetchFromAPI } from "../utils/fetchFromAPI";
 
 const VideoDetail = () => {
   const [videoDetail, setVideoDetail] = useState(null);
+  const [videos, setVideos] = useState(null);
+
   const { id } = useParams();
 
   useEffect(() => {
     FetchFromAPI(`videos?part=snippet,statistics&id=${id}`).then((data) =>
       setVideoDetail(data.items[0])
+    );
+
+    FetchFromAPI(`search?part=snippet&relatedToVideoId=${id}&type=video`).then(
+      (data) => setVideos(data.items)
     );
   }, [id]);
 
@@ -65,6 +71,14 @@ const VideoDetail = () => {
               </Stack>
             </Stack>
           </Box>
+        </Box>
+        <Box
+          px={2}
+          py={{ md: 1, xs: 5 }}
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Videos videos={videos} direction="column" />
         </Box>
       </Stack>
     </Box>
